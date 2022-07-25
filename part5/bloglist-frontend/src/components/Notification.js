48f31b75acf7a2674207b06153f5f3a024fde8ef
tinojdev <tinojoonatan@gmail.com>
2022-07-25 18:49:08 +0300

    Added part5

--- part5/bloglist-frontend/src/components/Notification.js
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -14,6 +14,6 @@ const Notification = ({ message, isSuccess }) => {
 };
 Notification.propTypes = {
     message: PropTypes.string.isRequired,
-    isSuccess: PropTypes.bool.isRequired,
+    isSuccess: PropTypes.bool,
 };
 export default Notification;
